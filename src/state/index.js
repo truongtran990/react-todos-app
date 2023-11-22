aefb9cc2b209b52e404814fddb6246390d75ab4b
VanTruongUIT <tvtruong.uit@gmail.com>
2023-11-22 23:39:20 +0700

    create basic example with store and reducer in redux

--- src/state/index.js
+++ src/state/index.js
@@ -22,3 +22,22 @@ const counterReducer = (state = { value: 0 }, action) => {
       return state;
   }
 }
+
+// Create a Redux store holding the state of your app
+// Its API is {subscribe, dispatch, getState}.
+let store = createStore(counterReducer);
+
+
+// You can use subcribe() to update the UI in response to state changes.
+// Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
+// There may be additional use cases where it's helpful to subscribe as well.
+store.subscribe(() => console.log(store.getState()));
+
+
+// The only way to mutate the internal state is to dispatch an action.
+// The actions can be serialized, logged or stored and later replayed
+store.dispatch({ type: 'counter/incremented' }) // {value: 1}
+
+store.dispatch({ type: 'counter/incremented' }) // {value: 2}
+
+store.dispatch({ type: 'counter/decremented' }) // {value: 1}
